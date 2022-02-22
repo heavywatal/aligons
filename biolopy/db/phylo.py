b2573550e3ca3dcb1db8e9b76641dc52e40d3931
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-02-22 19:22:49 +0900

    :sparkles: Add --graph to db.phylo for tree view

--- biolopy/db/phylo.py
+++ biolopy/db/phylo.py
@@ -1,6 +1,10 @@
+from __future__ import annotations
+
 import argparse
 import logging
 import re
+from collections.abc import Callable, Generator, Iterable
+from typing import NamedTuple, TypeAlias
 
 from .. import cli
 
@@ -11,6 +15,7 @@ def main(argv: list[str] = []):
     parser = argparse.ArgumentParser(parents=[cli.logging_argparser()])
     parser.add_argument("-n", "--name", action="store_true")
     parser.add_argument("-s", "--short", action="store_true")
+    parser.add_argument("-g", "--graph", action="count")
     parser.add_argument("clade", nargs="?")
     args = parser.parse_args(argv or None)
     cli.logging_config(args.loglevel)
@@ -21,6 +26,15 @@ def main(argv: list[str] = []):
         tree = trees[args.clade]
         if args.name:
             print(" ".join(extract_labels(tree)))
+        elif args.graph:
+            newick = trees[args.clade]
+            root = parse_newick(newick)
+            if args.graph > 1:
+                gen = render_nodes(root)
+            else:
+                gen = render_tips(root)
+            for branch, label in gen:
+                print(f"{branch} {label}")
         else:
             print(tree)
         return
@@ -66,6 +80,110 @@ def make_trees():
 
 
 trees = make_trees()
+
+
+class Node(NamedTuple):
+    name: str
+    children: list[Node] = []
+    distance: float | None = None
+
+
+class Branches(NamedTuple):
+    fork: str = "├─"
+    straight: str = "│ "
+    corner: str = "└─"
+    blank: str = "  "
+
+
+StrGen: TypeAlias = Generator[str, None, None]
+GraphGen: TypeAlias = Generator[tuple[str, str], None, None]
+BRANCHES = Branches()
+COMPACT_BRANCHES = Branches("┬─")
+
+
+def render_nodes(node: Node, columns: list[StrGen] = []) -> GraphGen:
+    for line in node.name.splitlines():
+        prefix = "".join([next(gen) for gen in columns])
+        yield (prefix, line)
+    for x in _iter_children(node, columns, render_nodes, BRANCHES):
+        yield x
+
+
+def render_tips(node: Node, columns: list[StrGen] = []) -> GraphGen:
+    for x in _iter_children(node, columns, render_tips, COMPACT_BRANCHES):
+        yield x
+    if not node.children:
+        prefix = "".join([next(gen) for gen in columns])
+        yield (prefix, node.name)
+
+
+def _iter_children(
+    node: Node,
+    columns: list[StrGen],
+    render_fun: Callable[[Node, list[StrGen]], GraphGen],
+    branches: Branches,
+):
+    for child in (children := node.children):
+        if child != children[-1]:
+            gen = _column_generator(branches.fork, branches.straight)
+        else:
+            gen = _column_generator(branches.corner, branches.blank)
+        columns.append(gen)
+        for x in render_fun(child, columns):
+            yield x
+        columns.pop()
+
+
+def _column_generator(first: str, rest: str) -> StrGen:
+    yield first
+    while True:
+        yield rest
+
+
+def parse_newick(newick: str):
+    nodes = {}
+    newick_old = ""
+    while newick != newick_old:
+        newick_old = newick
+        newick, nodes = _extract_tip_trios(newick, nodes)
+    root = nodes.popitem()[1]
+    assert not nodes
+    return root
+
+
+def _extract_tip_trios(tree: str, nodes: dict[str, Node]):
+    for mobj in re.finditer(r"\(([^(]+?),([^(]+?)\)([^(),;]+)?", tree):
+        name1 = mobj.group(1)
+        name2 = mobj.group(2)
+        node1 = nodes.pop(name1, _make_node(name1))
+        node2 = nodes.pop(name2, _make_node(name2))
+        name3 = f"{node1.name}+{node2.name}" + (mobj.group(3) or "")
+        nodes[name3] = _make_node(name3, [node1, node2])
+        tree = tree.replace(mobj.group(0), name3)
+    return tree, nodes
+
+
+def _make_node(label: str, children: list[Node] = []) -> Node:
+    if ":" in label:
+        (name, dist) = label.split(":")
+        return Node(name.strip(), children, float(dist.strip()))
+    return Node(label.strip(), children)
+
+
+def rectangulate(renderer: Iterable[tuple[str, str]]):
+    lines = list(renderer)
+    widest = max(lines, key=lambda p: len(p[0]) + len(p[1]))
+    max_width = len(widest[0]) + len(widest[1])
+    for prefix, label in lines:
+        yield (prefix.ljust(max_width - len(label), "─"), label)
+
+
+def elongate(renderer: Iterable[tuple[str, str]]):
+    lines = list(renderer)
+    deepest = max(lines, key=lambda p: len(p[0]))
+    max_depth = len(deepest[0])
+    for prefix, label in lines:
+        yield (prefix.ljust(max_depth, "─"), label)
 
 
 if __name__ == "__main__":
--- biolopy/db/stat.py
+++ biolopy/db/stat.py
@@ -3,7 +3,7 @@ import csv
 import logging
 
 from .. import cli
-from . import ensemblgenomes
+from . import ensemblgenomes, phylo
 
 _log = logging.getLogger(__name__)
 
@@ -11,29 +11,29 @@ _log = logging.getLogger(__name__)
 def main(argv: list[str] | None = None):
     parser = argparse.ArgumentParser(parents=[cli.logging_argparser()])
     parser.add_argument("-n", "--dry-run", action="store_true")
+    parser.add_argument("-c", "--clade", default="monocot")
     args = parser.parse_args(argv or None)
     cli.dry_run = args.dry_run
     cli.logging_config(args.loglevel)
-    genome_size()
-    genome_gff3_size()
+    newick = phylo.trees[args.clade]
+    root = phylo.parse_newick(newick)
+    for pre, species in phylo.rectangulate(phylo.render_tips(root)):
+        fasize, nseqs = chrom_sizes(species)
+        gffsize = gff3_size(species)
+        print(f"{pre} {species} {fasize:4.0f}Mbp {nseqs:2} {gffsize:4.1f}MB")
 
 
-def genome_size():
-    for x in sorted(ensemblgenomes.rglob("fasize.chrom.sizes")):
-        species = x.parent.parent.name
-        with open(x, "rt") as fin:
-            reader = csv.reader(fin, delimiter="\t")
-            lengths = [int(row[1]) for row in reader]
-        num_seqs = len(lengths)
-        total_length = round(sum(lengths) / 1e6)
-        print(f"{species:24} {total_length:5,}Mbp {num_seqs:2}")
+def chrom_sizes(species: str):
+    path = ensemblgenomes.get_file("fasize.chrom.sizes", species)
+    with open(path, "rt") as fin:
+        reader = csv.reader(fin, delimiter="\t")
+        lengths = [int(row[1]) for row in reader]
+    return sum(lengths) / 1e6, len(lengths)
 
 
-def genome_gff3_size():
-    for x in sorted(ensemblgenomes.rglob("*.genome.gff3.gz")):
-        species = x.parent.name
-        size = round(x.stat().st_size / 1e3)
-        print(f"{species:24} {size:6,}KB")
+def gff3_size(species: str):
+    path = ensemblgenomes.get_file("*.genome.gff3.gz", species)
+    return path.stat().st_size / 1e6
 
 
 if __name__ == "__main__":
