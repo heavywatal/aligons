57039d04adfb961d2d2051c47bd566a4388959a8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-07-03 22:26:16 +0900

    :white_check_mark: Improve coverage for util.fs

--- tests/util/test_fs.py
+++ tests/util/test_fs.py
@@ -1,3 +1,4 @@
+import pytest
 from pathlib import Path
 
 from aligons.util import fs
@@ -10,15 +11,17 @@ def test_is_outdated(tmp_path: Path):
     assert fs.is_outdated(tmp_file) and tmp_file.exists()
     this_file = Path(__file__)
     caches = list((this_file.parent / "__pycache__").iterdir())
-    latest_cache = fs.newest(caches)
-    assert fs.is_outdated(this_file, latest_cache) and caches
+    assert fs.is_outdated(this_file, caches) and caches
     assert not fs.is_outdated(this_file)
 
 
-def test_sorted_naturally():
+def test_sorted_naturally(capsys: pytest.CaptureFixture[str]):
     names = ["chr.1.fa", "chr.2.fa", "chr.10.fa", "chr.Mt.fa"]
     files = [Path(x) for x in names]
     assert sorted(files) != files
     assert fs.sorted_naturally(names) == names
     assert fs.sorted_naturally(files) == files
     assert fs.sorted_naturally(reversed(files)) == files
+    fs.main(names)
+    captured = capsys.readouterr()
+    assert captured.out == "\n".join(names) + "\n"
