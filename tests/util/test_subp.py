91705be9fb9a869a0e0c92640c678f1871e8ee7f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2022-09-23 21:01:08 +0900

    :white_check_mark: Set dry_run = False before testing subp

--- tests/util/test_subp.py
+++ tests/util/test_subp.py
@@ -1,11 +1,12 @@
 import pytest
-from aligons.util import subp
+from aligons.util import cli, subp
 
 hello = "printf hello"
 
 
 @pytest.mark.parametrize("cmd", [hello, hello.split()])
 def test_run(cmd: str | list[str]):
+    cli.dry_run = False
     assert subp.run_if(False, cmd, stdout=subp.PIPE).stdout == b""
     assert subp.run_if(True, cmd, stdout=subp.PIPE).stdout == b"hello"
     assert subp.run(cmd, stdout=subp.PIPE).stdout == b"hello"
@@ -13,6 +14,7 @@ def test_run(cmd: str | list[str]):
 
 @pytest.mark.parametrize("cmd", [hello, hello.split()])
 def test_popen(cmd: str | list[str]):
+    cli.dry_run = False
     assert subp.popen_if(False, cmd, stdout=subp.PIPE).communicate()[0] == b""
     assert subp.popen_if(True, cmd, stdout=subp.PIPE).communicate()[0] == b"hello"
     assert subp.popen(cmd, stdout=subp.PIPE).communicate()[0] == b"hello"
