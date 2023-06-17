heavywatal/aligons

commit 24cb4a20937233e3c4cc2ed8cee39ddf45ecf78d

:boom: Change db structure
- Separate local mirrors and keep them untouched
- Support multiple data source, e.g., ensembl+solgenomics

src/aligons/db/plantregmap.py
@@ -9,35 +9,35 @@ from aligons.util import cli, fs
 from . import tools
 
 _log = logging.getLogger(__name__)
-HOST = "plantregmap.gao-lab.org"
+_HOST = "plantregmap.gao-lab.org"
 
 
 def main(argv: list[str] | None = None):
     parser = cli.ArgumentParser()
     parser.add_argument("-D", "--download", action="store_true")
-    parser.add_argument("species", nargs="?", default=".")
+    parser.add_argument("pattern", nargs="?", default="*")
     args = parser.parse_args(argv or None)
     if args.download:
         cli.wait_raise(retrieve_deploy(q) for q in iter_download_queries())
-    for x in fs.sorted_naturally(rglob("*.*", args.species)):
-        print(x)
-
-
-def rglob(pattern: str, species: str = "."):
-    for species_dir in local_db_root().iterdir():
-        if re.search(species, species_dir.name, re.IGNORECASE):
-            for x in species_dir.rglob(pattern):
-                yield x
+    else:
+        for x in fs.sorted_naturally(db_prefix().rglob(args.pattern)):
+            print(x)
 
 
 def retrieve_deploy(query: str):
-    url = f"http://{HOST}/download_ftp.php?{query}"
-    outfile = local_db_root() / query.split("/", 1)[1]
-    if outfile.suffix in (".bed", ".gff", ".txt"):
-        outfile = outfile.parent / (outfile.name + ".gz")
+    url = f"http://{_HOST}/download_ftp.php?{query}"
+    relpath = query.split("/", 1)[1]
+    rawfile = db.path_mirror(_HOST) / relpath
+    outfile = db_prefix() / relpath
+    if outfile.suffix in (".bed", ".gff"):
+        outfile = outfile.with_suffix(outfile.suffix + ".gz")
     elif outfile.name.endswith(".gtf.gz"):
         outfile = outfile.with_suffix("").with_suffix(".gff.gz")
-    future = tools.retrieve_compress(url, outfile)
+    content = tools.retrieve_content(url, rawfile)
+    if outfile.suffix == ".gz":
+        future = cli.thread_submit(tools.compress, content, outfile)
+    else:
+        future = cli.thread_submit(fs.symlink, rawfile, outfile)
     return cli.thread_submit(htslib.try_index, future)
 
 
@@ -54,12 +54,12 @@ def iter_download_queries_all():
 
 
 def download_php():
-    url = f"http://{HOST}/download.php"
-    cache = local_db_root() / "download.php.html"
-    return tools.retrieve_cache(url, cache).decode()
+    url = f"http://{_HOST}/download.php"
+    cache = db.path_mirror(_HOST) / "download.php.html"
+    return tools.retrieve_content(url, cache, force=True).decode()
 
 
-def local_db_root():
+def db_prefix():
     return db.path("plantregmap")
 
 

src/aligons/extern/jbrowse.py
@@ -1,7 +1,7 @@
 """https://jbrowse.org.
 
-src: {db.root}/aligons/{label}/fasta/{species}/*.fa.gz
-src: {db.root}/aligons/{label}/gff3/{species}/*.chr.gff3.gz
+src: {db.api.prefix}/fasta/{species}/*.fa.gz
+src: {db.api.prefix}/gff3/{species}/*.chr.gff3.gz
 src: {vNN}/pairwise/{species}/{query}/cram/genome.cram
 src: {vNN}/multiple/{species}/{clade}/phastcons.bw
 dst: {vNN}/{jbrowse_XYZ}/{species}/config.json

src/aligons/data/config.toml
@@ -34,8 +34,9 @@ expected-length = 12
 version = 49
 
 [db]
-root = "~/db"
-label = "default"
+root = "~/db/aligons"
+mirror = "~/db"
+origin = ["ensembl-{version}", "solgenomics"]
 kmer = false
 
 [jellyfish.count]

src/aligons/db/__init__.py
@@ -10,3 +10,7 @@ def _expand_path(s: str):
 
 def path(relpath: str | Path = ""):
     return _expand_path(config["db"]["root"]) / relpath
+
+
+def path_mirror(relpath: str | Path = ""):
+    return _expand_path(config["db"]["mirror"]) / relpath

src/aligons/db/api.py
@@ -1,6 +1,6 @@
 """Interface to working data.
 
-prefix: {db.root}/aligons/{db.label}/
+prefix: {db.root}/{origin}/
 
 - fasta/{species}/
 - gff3/{species}/
@@ -12,6 +12,7 @@ from contextlib import suppress
 from pathlib import Path
 
 from aligons import db
+from aligons.db import ensemblgenomes
 from aligons.util import cli, config, fs
 
 _log = logging.getLogger(__name__)
@@ -21,17 +22,12 @@ def main(argv: list[str] | None = None):
     parser = cli.ArgumentParser()
     parser.add_argument("-S", "--species")
     args = parser.parse_args(argv or None)
-    print(prefix())
     if args.species:
         for p in _glob("*", args.species):
-            print(p.relative_to(prefix()))
+            print(p)
     else:
         for p in _species_dirs():
             print(p)
-
-
-def prefix() -> Path:
-    return db.path("aligons") / config["db"]["label"]
 
 
 @functools.cache
@@ -40,22 +36,22 @@ def species_names(fmt: str = "fasta"):
 
 
 def fasize(species: str) -> Path:
-    return _get_file("fasize.chrom.sizes", species)
+    return get_file("fasize.chrom.sizes", species)
 
 
 def genome_fa(species: str) -> Path:
     subdir = "kmer" if config["db"]["kmer"] else ""
-    return _get_file("*.genome.fa.gz", species, subdir)
+    return get_file("*.genome.fa.gz", species, subdir)
 
 
 def genome_2bit(species: str) -> Path:
     subdir = "kmer" if config["db"]["kmer"] else ""
-    return _get_file("*.genome.2bit", species, subdir)
+    return get_file("*.genome.2bit", species, subdir)
 
 
 def genome_gff3(species: str) -> Path:
     subdir = "kmer" if config["db"]["kmer"] else ""
-    return _get_file("*.genome.gff3.gz", species, subdir)
+    return get_file("*.genome.gff3.gz", species, subdir)
 
 
 def list_chromosome_fa(species: str) -> Iterable[Path]:
@@ -72,6 +68,12 @@ def list_chromosome_gff3(species: str) -> Iterable[Path]:
     return fs.sorted_naturally(_glob("*.chromosome*.gff3.gz", species))
 
 
+def get_file(pattern: str, species: str, subdir: str = ""):
+    found = list(_glob(pattern, species, subdir))
+    assert len(found) == 1, found
+    return found[0]
+
+
 def sanitize_queries(target: str, queries: list[str]):
     queries = list(dict.fromkeys(queries))
     with suppress(ValueError):
@@ -83,23 +85,29 @@ def sanitize_queries(target: str, queries: list[str]):
 
 
 def _species_dirs(fmt: str = "fasta") -> Iterable[Path]:
-    assert (root := prefix() / fmt).exists(), root
-    for path in root.iterdir():
-        if path.is_dir():
-            yield path
-
-
-def _get_file(pattern: str, species: str, subdir: str = ""):
-    found = list(_glob(pattern, species, subdir))
-    assert len(found) == 1, found
-    return found[0]
+    for prefix in _iter_prefix():
+        if not (fmt_dir := prefix / fmt).exists():
+            _log.warning(f"{fmt_dir} does not exist")
+            continue
+        for path in fmt_dir.iterdir():
+            if path.is_dir():
+                yield path
 
 
 def _glob(pattern: str, species: str, subdir: str = ""):
-    for fmt in ("fasta", "gff3"):
-        d = prefix() / fmt / species / subdir
-        for x in fs.sorted_naturally(d.glob(pattern)):
-            yield x
+    for prefix in _iter_prefix():
+        for fmt in ("fasta", "gff3"):
+            d = prefix / fmt / species / subdir
+            for x in fs.sorted_naturally(d.glob(pattern)):
+                yield x
+
+
+def _iter_prefix() -> Iterable[Path]:
+    return (_prefix(origin) for origin in config["db"]["origin"])
+
+
+def _prefix(origin: str) -> Path:
+    return db.path(origin.format(version=ensemblgenomes.version()))
 
 
 if __name__ == "__main__":

src/aligons/db/ensemblgenomes.py
@@ -1,10 +1,9 @@
 """https://plants.ensembl.org/.
 
-prefix: {db.root}/ensemblgenomes/plants/release-{version}
-
-- fasta/{species}/dna/{stem}.dna_sm.chromosome.{seqid}.fa.gz
-- gff3/{species}/
-- maf/ensembl-compara/pairwise_alignments/
+- {db.mirror}/ensemblgenomes.org/plants/release-{version}/
+  - fasta/{species}/dna/{stem}.dna_sm.chromosome.{seqid}.fa.gz
+  - gff3/{species}/
+  - maf/ensembl-compara/pairwise_alignments/
 """
 import functools
 import logging
@@ -79,8 +78,8 @@ def make_newicks():
 
 
 def list_versions():
-    _log.debug(f"{local_db_root()=}")
-    return local_db_root().glob("release-*")
+    _log.debug(f"{_prefix_mirror_root()=}")
+    return _prefix_mirror_root().glob("release-*")
 
 
 @functools.cache
@@ -96,7 +95,7 @@ def species_names(fmt: str = "fasta"):
 
 
 def species_dirs(fmt: str = "fasta", species: list[str] | None = None):
-    assert (root := prefix() / fmt).exists(), root
+    assert (root := _prefix_mirror() / fmt).exists(), root
     requests = set(species or [])
     for path in root.iterdir():
         if not path.is_dir():
@@ -222,14 +221,14 @@ class FTPensemblgenomes(FTP):
         path = f"/pub/plants/release-{version()}"
         _log.info(f"ftp.cwd({path})")
         _log.info(self.cwd(path))
-        _log.info(f"os.chdir({prefix()})")
+        _log.info(f"os.chdir({_prefix_mirror()})")
         self.orig_wd = Path.cwd()
-        prefix().mkdir(0o755, parents=True, exist_ok=True)
-        os.chdir(prefix())  # for RETR only
+        _prefix_mirror().mkdir(0o755, parents=True, exist_ok=True)
+        os.chdir(_prefix_mirror())  # for RETR only
 
     def download_fasta(self, species: str):
         relpath = f"fasta/{species}/dna"
-        outdir = prefix() / relpath
+        outdir = _prefix_mirror() / relpath
         nlst = self.nlst_cache(relpath)
         files = [self.retrieve(x) for x in self.remove_duplicates(nlst, "_sm.")]
         fs.checksums(outdir / "CHECKSUMS")
@@ -237,7 +236,7 @@ class FTPensemblgenomes(FTP):
 
     def download_gff3(self, species: str):
         relpath = f"gff3/{species}"
-        outdir = prefix() / relpath
+        outdir = _prefix_mirror() / relpath
         nlst = self.nlst_cache(relpath)
         files = [self.retrieve(x) for x in self.remove_duplicates(nlst)]
         fs.checksums(outdir / "CHECKSUMS")
@@ -245,7 +244,7 @@ class FTPensemblgenomes(FTP):
 
     def download_maf(self, species: str):
         relpath = "maf/ensembl-compara/pairwise_alignments"
-        outdir = prefix() / relpath
+        outdir = _prefix_mirror() / relpath
         nlst = self.nlst_cache(relpath)
         sp = shorten(species)
         for x in nlst:
@@ -254,7 +253,7 @@ class FTPensemblgenomes(FTP):
         _log.debug(f"{outdir=}")
         dirs: list[Path] = []
         for targz in outdir.glob("*.tar.gz"):
-            expanded = prefix() / targz.with_suffix("").with_suffix("")
+            expanded = _prefix_mirror() / targz.with_suffix("").with_suffix("")
             tar = ["tar", "xzf", targz, "-C", outdir]
             subp.run(tar, if_=fs.is_outdated(expanded / "README.maf"))
             # TODO: MD5SUM
@@ -277,7 +276,7 @@ class FTPensemblgenomes(FTP):
         return matched + misc
 
     def nlst_cache(self, relpath: str):
-        cache = prefix() / relpath / ".ftp_nlst_cache"
+        cache = _prefix_mirror() / relpath / ".ftp_nlst_cache"
         if cache.exists():
             _log.info(f"{cache=}")
             with cache.open("r") as fin:
@@ -293,7 +292,7 @@ class FTPensemblgenomes(FTP):
         return lst
 
     def retrieve(self, path: str):
-        outfile = prefix() / path
+        outfile = _prefix_mirror() / path
         if not outfile.exists() and not cli.dry_run:
             outfile.parent.mkdir(0o755, parents=True, exist_ok=True)
             with outfile.open("wb") as fout:
@@ -309,16 +308,20 @@ def rsync(relpath: str, options: str = ""):
     server = "ftp.ensemblgenomes.org"
     remote_prefix = f"rsync://{server}/all/pub/plants/release-{version()}"
     src = f"{remote_prefix}/{relpath}/"
-    dst = prefix() / relpath
+    dst = _prefix_mirror() / relpath
     return subp.run(f"rsync -auv {options} {src} {dst}")
 
 
 def prefix():
-    return local_db_root() / f"release-{version()}"
+    return db.path(f"ensembl-{version()}")
 
 
-def local_db_root():
-    return db.path("ensemblgenomes/plants")
+def _prefix_mirror():
+    return _prefix_mirror_root() / f"release-{version()}"
+
+
+def _prefix_mirror_root():
+    return db.path_mirror("ensemblgenomes.org/plants")
 
 
 def version():

src/aligons/db/plantdhs.py
@@ -9,6 +9,7 @@ from aligons.util import cli, fs
 from . import tools
 
 _log = logging.getLogger(__name__)
+_HOST = "plantdhs.org"
 
 
 def main(argv: list[str] | None = None):
@@ -18,20 +19,22 @@ def main(argv: list[str] | None = None):
     args = parser.parse_args(argv or None)
     if args.download:
         cli.wait_raise(retrieve_deploy(q) for q in iter_download_queries())
-    for x in fs.sorted_naturally(glob(args.pattern)):
-        print(x)
-
-
-def glob(pattern: str):
-    return local_db_root().glob(pattern)
+    else:
+        for x in fs.sorted_naturally(db_prefix().rglob(args.pattern)):
+            print(x)
 
 
 def retrieve_deploy(query: str):
     url = f"https://bioinfor.yzu.edu.cn/download/plantdhs/{query}"
-    outfile = local_db_root() / query
-    if outfile.name.endswith(".gff.zip"):
+    rawfile = db.path_mirror(_HOST) / query
+    outfile = db_prefix() / query
+    if outfile.name.endswith(".zip"):
         outfile = outfile.with_suffix(".gz")
-    future = tools.retrieve_compress(url, outfile)
+    content = tools.retrieve_content(url, rawfile)
+    if outfile.suffix == ".gz":
+        future = cli.thread_submit(tools.compress, content, outfile)
+    else:
+        future = cli.thread_submit(fs.symlink, rawfile, outfile)
     return cli.thread_submit(htslib.try_index, future)
 
 
@@ -48,12 +51,12 @@ def iter_download_queries_all():
 
 
 def download_page():
-    url = "http://plantdhs.org/Download"
-    cache = local_db_root() / "download.html"
-    return tools.retrieve_cache(url, cache).decode()
+    url = f"http://{_HOST}/Download"
+    cache = db.path_mirror(_HOST) / "Download.html"
+    return tools.retrieve_content(url, cache, force=True).decode()
 
 
-def local_db_root():
+def db_prefix():
     return db.path("plantdhs")
 
 

src/aligons/db/prep.py
@@ -6,7 +6,7 @@ from aligons.db import tools
 from aligons.extern import htslib, jellyfish, kent
 from aligons.util import cli, config, fs, read_config
 
-from . import api, ensemblgenomes, phylo
+from . import ensemblgenomes, phylo
 
 _log = logging.getLogger(__name__)
 
@@ -64,7 +64,7 @@ def symlink(path: Path, species: str, fmt: str = ""):
         fmt = "fasta" if path.name.removesuffix(".gz").endswith(".fa") else "gff3"
     assert fmt in ("fasta", "gff3")
     filename = path.name.replace("primary_assembly", "chromosome")
-    link = api.prefix() / fmt / species / filename
+    link = ensemblgenomes.prefix() / fmt / species / filename
     return fs.symlink(path, link)
 
 

commit 9499ad8e0ffae3d5bc23d5756aa9663a56b42302

:bug: Address minor regressions by 24cb4a2

src/aligons/db/plantregmap.py
@@ -63,5 +63,12 @@ def db_prefix():
     return db.path("plantregmap")
 
 
+def rglob(pattern: str, species: str = "."):
+    for species_dir in db_prefix().iterdir():
+        if re.search(species, species_dir.name, re.IGNORECASE):
+            for x in species_dir.rglob(pattern):
+                yield x
+
+
 if __name__ == "__main__":
     main()

src/aligons/extern/jbrowse.py
@@ -138,10 +138,10 @@ class JBrowseConfig:
             self.add_track(f, "papers", trackid="SV_all-qin2021", subdir="suzuemon")
 
     def add_plantdhs(self):
-        for path in fs.sorted_naturally(plantdhs.glob("Rice_*.bw")):
+        for path in fs.sorted_naturally(plantdhs.db_prefix().glob("Rice_*.bw")):
             trackid = path.stem.removeprefix("Rice_")
             self.add_track(path, "plantdhs", trackid=trackid)
-        for path in fs.sorted_naturally(plantdhs.glob("*.gff.gz")):
+        for path in fs.sorted_naturally(plantdhs.db_prefix().glob("*.gff.gz")):
             self.add_track(path, "plantdhs", trackid=path.stem)
 
     def add_plantregmap(self, species: str):