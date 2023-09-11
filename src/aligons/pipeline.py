32d687d378ae5cfc9902e31874b85bc8f02b9493
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-09-11 16:31:12 +0900

    :art: Create symlinks with clade names for backword compatibility

--- src/aligons/pipeline.py
+++ src/aligons/pipeline.py
@@ -41,6 +41,8 @@ def phastcons(target: str, clade: str, tips: int, max_bp: float, *, compara: boo
             continue
         _log.info(f"{species = }")
         multiple = multiz.run(pairwise, species)
+        if n == len(lst_species):
+            (multiple.parent / clade).symlink_to(multiple.name)
         phast.run(multiple)
         kent.run(multiple)
     cli.wait_raise(fts)
