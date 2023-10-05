d808f6729e818ea2f4032760359be0894d8fd12d
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-10-05 18:48:14 +0900

    :construction: Create alias scripts in apptainer.py

--- src/aligons/extern/apptainer.py
+++ src/aligons/extern/apptainer.py
@@ -10,40 +10,80 @@ from aligons.util import cli, dl, fs
 _log = logging.getLogger(__name__)
 _galaxy_domain = "depot.galaxyproject.org"
 _galaxy_prefix = f"https://{_galaxy_domain}/singularity/"
-_galaxy_apps = [
-    "bedtools",
-    "jbrowse2",
-    "jellyfish",
-    "lastz",
-    "multiz",
-    "phast",
-    "repeatmasker",
-    "samtools",
-    "seqkit",
-    "trf",
-]
+_galaxy_apps = {
+    "bedtools": [],
+    "jbrowse2": ["jbrowse"],
+    "jellyfish": [],
+    "last": ["maf-convert"],
+    "lastz": [],
+    "multiz": ["multiz", "roast", "maf_project"],
+    "phast": ["phastCons", "phyloP", "phyloFit", "phyloBoot", "msa_view"],
+    "repeatmasker": ["RepeatMasker", "/usr/local/share/RepeatMasker/famdb.py"],
+    "samtools": ["samtools", "bgzip", "tabix"],
+    "seqkit": [],
+    "trf": [],
+    "ucsc-axtchain": ["axtChain"],
+    "ucsc-axtsort": ["axtSort"],
+    "ucsc-axttomaf": ["axtToMaf"],
+    "ucsc-chainmergesort": ["chainMergeSort"],
+    "ucsc-chainnet": ["chainNet"],
+    "ucsc-chainprenet": ["chainPreNet"],
+    "ucsc-fasize": ["faSize"],
+    "ucsc-fatotwobit": ["faToTwoBit"],
+    "ucsc-netsyntenic": ["netSyntenic"],
+    "ucsc-nettoaxt": ["netToAxt"],
+    "ucsc-wigtobigwib": ["wigToBigWig"],
+}
 
 
 def main(argv: list[str] | None = None):
     parser = cli.ArgumentParser()
     parser.add_argument("-a", "--all", action="store_true")
     parser.add_argument("-D", "--download", action="store_true")
+    parser.add_argument("--prefix", type=Path, default=Path())
     args = parser.parse_args(argv or None)
-    tsv = galaxy_index()
     if args.all:
+        tsv = galaxy_index()
         print(tsv.open("rt").read())
-    else:
-        table = pl.read_csv(tsv, separator="\t")
-        for x in latest_apps(table):
-            url = f"{_galaxy_prefix}{x}"
-            if args.download:
-                dl.get(url)
-            print(x)
+    elif args.download:
+        pull_galaxy(args.prefix)
+
+
+def pull_galaxy(prefix: Path):
+    tsv = galaxy_index()
+    table = pl.read_csv(tsv, separator="\t")
+    imgdir = prefix / "biocontainers"
+    bindir = prefix / "bin"
+    if not cli.dry_run:
+        imgdir.mkdir(parents=True, exist_ok=True)
+        bindir.mkdir(parents=True, exist_ok=True)
+    for x in latest_apps(table):
+        url = f"{_galaxy_prefix}{x}"
+        sif = dl.get(url, imgdir / x).path
+        key = sif.name.split(":", 1)[0]
+        cmds = _galaxy_apps[key] or [key]
+        for command in cmds:
+            sh = make_sh(sif, command, bindir)
+            _log.info(f"{sh}")
+
+
+def make_sh(sif: Path, command: str = "", outdir: Path = Path()):
+    if not command:
+        command = sif.name.split(":", 1)[0]
+    name = Path(command).name
+    outfile = outdir / name
+    apptainer_exec = f"apptainer exec {sif.absolute()} {command} $@"
+    _log.debug(apptainer_exec)
+    if not cli.dry_run:
+        with outfile.open("w") as fout:
+            fout.write(f"#!/bin/sh\n{apptainer_exec}\n")
+        outfile.chmod(0o775)
+    return outfile
 
 
 def latest_apps(table: pl.DataFrame):
     return (
-        table.filter(pl.col("app").is_in(_galaxy_apps))
+        table.filter(pl.col("app").is_in(_galaxy_apps.keys()))
         .select(
             pl.concat_str([pl.col("app"), pl.col("tag")], separator=":").alias("name")
         )
