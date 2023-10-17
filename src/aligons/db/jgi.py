777788330f51c9d5b5a90c92d40ce6b681314f0b
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2023-10-17 18:08:51 +0900

    :construction: Create db.jgi for authenticated session

---
+++ src/aligons/db/jgi.py
@@ -0,0 +1,106 @@
+import logging
+import re
+from pathlib import Path
+from xml.etree import ElementTree
+
+from aligons import db
+from aligons.util import cli, config, dl, tomli_w
+
+_log = logging.getLogger(__name__)
+_HOST = "genome.jgi.doe.gov"
+
+session = dl.LazySession(
+    "https://signon.jgi.doe.gov/signon/create",
+    data={
+        "login": config["jgi"]["login"],
+        "password": config["jgi"]["password"],
+    },
+)
+
+
+def main(argv: list[str] | None = None):
+    parser = cli.ArgumentParser()
+    parser.add_argument("-D", "--download", action="store_true")
+    _args = parser.parse_args(argv or None)
+    for x in ["PhytozomeV11", "PhytozomeV12"]:
+        make_dataset_toml(x)
+
+
+def make_dataset_toml(organism: str = "PhytozomeV12"):
+    outfile = db.path(_HOST) / (organism + ".toml")
+    _log.info(f"{outfile}")
+    if not outfile.exists():
+        xml = fetch_xml(organism).path
+        _log.info(f"{xml}")
+        tree = ElementTree.parse(xml)  # noqa: S314
+        dataset = gather_dataset(tree.getroot())
+        if not cli.dry_run:
+            outfile.parent.mkdir(0o755, exist_ok=True)
+            with outfile.open("wb") as fout:
+                tomli_w.dump(dataset, fout)
+    return outfile
+
+
+def gather_dataset(root: ElementTree.Element):
+    datasets: list[dict[str, str | list[str]]] = []
+    for efolder in iter_species_folder(root):
+        d = as_dict(efolder)
+        if d:
+            datasets.append(d)
+    return {"dataset": datasets}
+
+
+def as_dict(efolder: ElementTree.Element) -> dict[str, str | list[str]]:
+    _log.debug(f"{efolder.attrib}")
+    elem_assem = search_filename(efolder, r"softmasked\.fa\.gz$")
+    elem_annot = search_filename(efolder, r"repeatmasked.+\.gff3\.gz$")
+    if not elem_assem or not elem_annot:
+        return {}
+    fa_url = _simplify_url(elem_assem.attrib["url"])
+    gff_url = _simplify_url(elem_annot.attrib["url"])
+    species, version = parse_filename_fa(Path(fa_url).name)
+    return {
+        "url_prefix": f"https://{_HOST}",
+        "species": species,
+        "version": version,
+        "label": species,
+        "clade": "",
+        "sequences": [fa_url],
+        "annotation": gff_url,
+    }
+
+
+def _simplify_url(url: str):
+    return url.split("&url=", 1)[1]
+
+
+def iter_species_folder(root: ElementTree.Element):
+    _log.info(f"{root.attrib}")
+    for elem in root.iter("folder"):
+        if re.match("^[A-Z]", elem.attrib["name"]):
+            yield elem
+
+
+def parse_filename_fa(name: str):
+    stem = name.split(".softmasked")[0]
+    return stem.split("_", 1)
+
+
+def search_filename(
+    folder: ElementTree.Element, pattern: str
+) -> ElementTree.Element | None:
+    for elem in folder.iter("file"):
+        if re.search(pattern, elem.attrib["filename"]):
+            return elem
+    return None
+
+
+def fetch_xml(organism: str = "PhytozomeV12"):
+    outfile = db.path_mirror(_HOST) / (organism + ".xml")
+    query = f"?organism={organism}"
+    url = f"https://{_HOST}/portal/ext-api/downloads/get-directory"
+    return session.fetch(url + query, outfile)
+
+
+if __name__ == "__main__":
+    main()
--- src/aligons/db/tools.py
+++ src/aligons/db/tools.py
@@ -5,7 +5,7 @@ from collections.abc import Iterator
 from pathlib import Path
 
 from aligons import db
-from aligons.db import DataSet, mask
+from aligons.db import DataSet, jgi, mask
 from aligons.extern import htslib, jellyfish, kent
 from aligons.util import cli, dl, fs, gff, resources_data, tomllib
 
@@ -56,6 +56,8 @@ def fetch_and_bgzip(entry: DataSet, prefix: Path) -> list[cli.FuturePath]:
 
 
 def dl_mirror_db(url: str) -> dl.Response:
+    if "jgi.doe.gov" in url:
+        return jgi.session.mirror(url, db.path_mirror())
     return dl.mirror(url, db.path_mirror())
 
 
