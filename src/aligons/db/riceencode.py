9336c80fd1a59386c66eef3b640dd1334236ceb8
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-01-29 18:19:45 +0900

    :construction: Add db.riceencode

---
+++ src/aligons/db/riceencode.py
@@ -0,0 +1,61 @@
+"""glab.hzau.edu.cn/RiceENCODE/."""
+
+import logging
+import re
+from collections.abc import Iterable
+from pathlib import Path
+
+from aligons.util import cli, dl, fs
+
+from . import _rsrc, api, tools
+
+_log = logging.getLogger(__name__)
+_HOST = "glab.hzau.edu.cn"
+
+
+def main(argv: list[str] | None = None) -> None:
+    parser = cli.ArgumentParser()
+    parser.add_argument("-D", "--download", action="store_true")
+    parser.add_argument("pattern", nargs="?", default="*")
+    args = parser.parse_args(argv or None)
+    if args.download:
+        cli.wait_raise(retrieve_deploy(q) for q in iter_download_queries())
+    else:
+        for x in fs.sorted_naturally(db_prefix().rglob(args.pattern)):
+            fs.print_if_exists(x)
+
+
+def retrieve_deploy(query: str) -> cli.Future[Path]:
+    slug = f"RiceENCODE/download/{query}"
+    url = f"http://{_HOST}/{slug}"
+    rawfile = _rsrc.db_root(_HOST) / slug
+    outfile = db_prefix() / query
+    if outfile.name.endswith(".bigWig"):
+        outfile = outfile.with_suffix(".bw")
+    elif outfile.name.endswith(".bed"):
+        outfile = outfile.with_suffix(".bed.gz")
+    response = dl.fetch(url, rawfile)
+    return cli.thread_submit(tools.bgzip_or_symlink, response, outfile)
+
+
+def iter_download_queries() -> Iterable[str]:
+    content = download_page()
+    content = re.sub(r"<!--.+?-->", "", content)
+    content = re.sub(r"(/MH63/OpenChromatin/\w+).bw", r"\1.bigWig", content)
+    for mobj in re.finditer(r"href=\"\.\./download/([^\"]+)", content):
+        yield mobj[1]
+
+
+def download_page() -> str:
+    slug = "RiceENCODE/pages/download.html"
+    url = f"http://{_HOST}/{slug}"
+    cache = _rsrc.db_root(_HOST) / slug
+    return dl.fetch(url, cache).text
+
+
+def db_prefix() -> Path:
+    return api.prefix("riceencode")
+
+
+if __name__ == "__main__":
+    main()
--- src/aligons/db/tools.py
+++ src/aligons/db/tools.py
@@ -141,7 +141,9 @@ def bgzip_or_symlink(infile: Path | dl.Response, outfile: Path) -> Path:
         outfile.parent.mkdir(0o755, parents=True, exist_ok=True)
         if htslib.to_be_bgzipped(outfile.name):
             index_bgzip(infile, outfile)
-        elif outfile.suffix == infile.suffix:
+        elif outfile.suffix == infile.suffix or (
+            outfile.suffix == ".bw" and infile.suffix.lower() == ".bigwig"
+        ):
             fs.symlink(infile, outfile, relative=True)
         elif outfile.suffix == ".gz":
             _log.debug(f"rare case: gzip {infile = } to {outfile = }")
