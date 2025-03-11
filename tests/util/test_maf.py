def453ee5a7d189cd9737159d96065aa14abb5a7
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-03-11 16:02:48 +0900

    :white_check_mark: Test maf.read_bed() and to_one_based_inclusive()

--- tests/util/test_maf.py
+++ tests/util/test_maf.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from polars.testing import assert_series_equal
+
 from aligons.util import fs, maf
 
 data_dir = Path(__file__).parent.parent / "_data"
@@ -13,3 +15,18 @@ def test_maf_block_ranges(tmp_path: Path):
     tmp_bed = maf.maf_block_ranges(tmp_maf)
     with tmp_bed.open() as obs, expected_bed.open() as exp:
         assert obs.read() == exp.read()
+
+
+def test_read_bed_and_to_one():
+    expected_bed = data_dir / "ucsc.bed"
+    lf0 = maf.read_bed(expected_bed)
+    lf1 = maf.to_one_based_inclusive(lf0)
+    df0 = lf0.collect()
+    df1 = lf1.collect()
+    # depends on strand, but currently it is "+" for all records
+    assert_series_equal(df0.get_column("chrom"), df1.get_column("chrom"))
+    assert_series_equal(df0.get_column("start") + 1, df1.get_column("start"))
+    assert_series_equal(df0.get_column("end"), df1.get_column("end"))
+    assert_series_equal(df0.get_column("name"), df1.get_column("name"))
+    assert_series_equal(df0.get_column("score"), df1.get_column("score"))
+    assert_series_equal(df0.get_column("strand"), df1.get_column("strand"))
