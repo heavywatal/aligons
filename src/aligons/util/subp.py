97d2ce647b05050f200672bc7dd92bfe2189e22f
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2025-08-31 21:25:42 +0900

    docs: Write docs for subp module

--- src/aligons/util/subp.py
+++ src/aligons/util/subp.py
@@ -1,3 +1,5 @@
+"""Wrappers for [`subprocess`](https://docs.python.org/3/library/subprocess.html)."""
+
 import logging
 import os
 import re
@@ -31,6 +33,17 @@ def popen(  # noqa: PLR0913
     shell: bool = False,
     quiet: bool = False,
 ) -> Popen[bytes]:
+    """Wrap [`subprocess.Popen`](https://docs.python.org/3/library/subprocess.html#subprocess.Popen).
+
+    :param args: Command and arguments to execute.
+    :param if_: Passed to `prepare_args()`.
+    :param executable: Passed to `subprocess.Popen()`.
+    :param stdin: Passed to `subprocess.Popen()`.
+    :param stdout: Passed to `subprocess.Popen()`.
+    :param shell: Passed to `subprocess.Popen()`.
+    :param quiet: Decrease log level to `DEBUG` if `True`.
+    :return: A `Popen` object.
+    """
     # kwargs hinders type inference of output type [str | bytes]
     (args, cmd) = prepare_args(args, if_=if_)
     if quiet:
@@ -61,6 +74,22 @@ def run(  # noqa: PLR0913
     quiet: bool = False,
     check: bool = True,
 ) -> CompletedProcess[Any]:
+    """Wrap [`subprocess.run`](https://docs.python.org/3/library/subprocess.html#subprocess.run).
+
+    :param args: Command and arguments to execute.
+    :param if_: Passed to `prepare_args()`.
+    :param executable: Passed to `subprocess.run()`.
+    :param stdin: Passed to `subprocess.run()`.
+    :param input: Passed to `subprocess.run()`.
+    :param stdout: Passed to `subprocess.run()`.
+    :param stderr: Passed to `subprocess.run()`.
+    :param shell: Passed to `subprocess.run()`.
+    :param cwd: Passed to `subprocess.run()`.
+    :param text: Passed to `subprocess.run()`.
+    :param quiet: Decrease log level to `DEBUG` if `True`.
+    :param check: Passed to `subprocess.run()`.
+    :return: A `CompletedProcess` object.
+    """
     # kwargs hinders type inference of output type [str | bytes]
     (args, cmd) = prepare_args(args, if_=if_)
     if quiet:
@@ -84,6 +113,13 @@ def run(  # noqa: PLR0913
 
 
 def prepare_args(args: _CMD, *, if_: bool) -> tuple[Sequence[str | Path], str]:
+    """Prepare argument list and concatenated command string.
+
+    :param args: Command and arguments to execute.
+    :param if_: Returns a no-op command if `False`.
+    :return: The first element is the split arguments for execution;
+             the second element is the concatenated command string for logging.
+    """
     if isinstance(args, str):
         cmd = args.rstrip()
         args = shlex.split(cmd)
@@ -96,6 +132,12 @@ def prepare_args(args: _CMD, *, if_: bool) -> tuple[Sequence[str | Path], str]:
 
 
 def optargs(conf: Mapping[str, Any], prefix: str = "--") -> list[str]:
+    """Convert a dictionary to a list of command-line options.
+
+    :param conf: A dictionary of options.
+    :param prefix: A prefix for each option.
+    :return: A list of command-line options.
+    """
     return list(_iter_optargs(conf, prefix))
 
 
@@ -110,12 +152,26 @@ def _iter_optargs(conf: Mapping[str, Any], prefix: str = "--") -> Iterator[str]:
 
 
 def open_(file: Path, mode: str, *, if_: bool = True) -> IO[Any]:
+    """Conditionally open a file.
+
+    :param file: A file path.
+    :param mode: Passed to `Path.open()`.
+    :param if_: Open `os.devnull` if `False`.
+    :return: An opened file object.
+    """
     if cli.dry_run or not if_:
         file = Path(os.devnull)
     return file.open(mode)
 
 
 def gzip(data: FILE | bytes, outfile: Path, *, if_: bool = True) -> Path:
+    """Compress data with `zstd --format=gzip`.
+
+    :param data: A file-like object or bytes to compress.
+    :param outfile: Output file path.
+    :param if_: Do nothing if `False`.
+    :return: The output file path.
+    """
     assert outfile.suffix == ".gz", outfile
     args = ["zstd", "--format=gzip"]
     if_ = if_ and bool(data)
@@ -130,6 +186,13 @@ def gzip(data: FILE | bytes, outfile: Path, *, if_: bool = True) -> Path:
 def popen_zcat(
     infile: Path | Sequence[Path], *, stdout: FILE = PIPE, if_: bool = True
 ) -> Popen[bytes]:
+    """View compressed file(s) with `zstdcat` or `unzip`.
+
+    :param infile: Input file path(s).
+    :param stdout: Passed to `popen()`.
+    :param if_: Do nothing if `False`.
+    :return: A `Popen` object.
+    """
     return popen(_zcat_args(infile), stdout=stdout, if_=if_, quiet=True)
 
 
@@ -140,6 +203,14 @@ def run_zcat(
     stdout: FILE = PIPE,
     if_: bool = True,
 ) -> CompletedProcess[bytes]:
+    """View compressed file(s) with `zstdcat` or `unzip`.
+
+    :param infile: Input file path(s).
+    :param outfile: Output file path.
+    :param stdout: Passed to `run()`. Ignored if `outfile` is given.
+    :param if_: Do nothing if `False`.
+    :return: A `CompletedProcess` object.
+    """
     args = _zcat_args(infile)
     if outfile is not None:
         if args[0] == "unzip":
@@ -166,6 +237,15 @@ def popen_sd(
     stdout: FILE = PIPE,
     if_: bool = True,
 ) -> Popen[bytes]:
+    """Run [`sd`](https://github.com/chmln/sd) command.
+
+    :param pattern: Pattern to search.
+    :param repl: Replacement string.
+    :param stdin: Passed to `popen()`.
+    :param stdout: Passed to `popen()`.
+    :param if_: Passed to `popen()`
+    :return: A `Popen` object.
+    """
     if pattern:
         return popen(["sd", pattern, repl], stdin=stdin, stdout=stdout, if_=if_)
     assert not repl, repl
@@ -180,4 +260,13 @@ def run_sd(
     stdout: FILE = PIPE,
     if_: bool = True,
 ) -> CompletedProcess[bytes]:
+    """Run [`sd`](https://github.com/chmln/sd) command.
+
+    :param pattern: Pattern to search.
+    :param repl: Replacement string.
+    :param stdin: Passed to `run()`.
+    :param stdout: Passed to `run()`.
+    :param if_: Passed to `run()`.
+    :return: A `CompletedProcess` object.
+    """
     return run(["sd", pattern, repl], stdin=stdin, stdout=stdout, if_=if_)
